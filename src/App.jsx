24437dd9e2cf816570f9a6125a25f5dcb69e8b40
MariaCambrano <cambranoro10@gmail.com>
2026-09-23 13:22:25 -0600

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,12 @@
-import { useEffect, useState } from "react";
+import EjemploArreglo from "./componentes/EjemploArreglo";
+import Pila from "./componentes/Pila";
 
-function App() {
-  //Iniciamos con un estado para un arreglo
-  const [elementos, setEelementos]=useState([]);
-
-  //crear funcion para agregar datos
-  const agregarDato=()=>{
-    const nuevoNumero = Math.floor(Math.random()*50);
-    setEelementos([...elementos, nuevoNumero]);
-  };
-
-  //Metodo para recorrer el arreglo
-  const recorrerArreglo=(elemento,index)=>(
-    <li key={index} style={{margin: '5px 0',
-    fontsize:'18px'}}>
-      Elemento #{index+1}:<strong>{elemento}</strong>
-      </li>
-  )
-  
-
-  //Hook de efecto
-  useEffect(()=>{
-console.log("El arreglo de datos actual es: ",elementos);
-  },[elementos])
-  return (
+function App(){
+  return(
     <>
-    <h1>Mi primer arreglo de datos</h1>
-    <div style={{padding:'20px'}}>
-    <h2>Paso 1. Agregar datos al arreglo</h2>
-    <button onClick={agregarDato}>Agregar numero aleatorio</button>
-    <ul>
-      {/* Si elarreglo esta vacio  enviar un mensaje */}
-
-      {elementos.length===0?(
-        <>
-        <p>Aun no hay elementos en el arreglo</p>
-        <p>Presiona el boton agregar datos</p>
-        </>
-      ):(elementos.map(recorrerArreglo))}
-      
-      </ul>
-    </div>
+    <EjemploArreglo/>
     </>
-  )
-}
+  );
 
-export default App
+}
+export default App;
---
+++ src/componentes/Pila.jsx
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+
+function Pila() {
+    const [stack, setStack] = useState([]);
+    const [inputValue, setInputValue]=useState("");
+
+    //Agregar valores a la pila o arreglo
+    const handlePush=(e)=>{
+        e.preventDefault();
+        if(inputValue.trim()==="") return;
+        setStack([inputValue, ...stack]);
+        setInputValue('');
+    }
+
+    //Eliminar valores del arreglo pop
+    const handlePop=()=>{
+        if(stack.length===0) return;
+
+        //Filtrar los elementos y eliminamos el tope de la pila
+        const nuevoStack=stack.slice(1);
+        setStack(nuevoStack);
+    }
+
+    const elementoTope=stack.length>0 ? stack[0]:'La pila esta vacia';
+  return (
+    <>
+<div style={{padding: '20px', fontFamily:'Arial, sans-serif', maxWidth:'400px', margin:'0 auto'}}>
+    <h2>Visuzalizar los datos de la Pila</h2>
+    {/*Inicializamos el formulario para introducir datos */}
+    <form>
+        <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} placeholder='Introduce un dato' style={{padding:'8px', marginRight:'10px'}}/>
+    <button type='submit' style={{padding:'8px 12px', background: '#4CAF50'}}></button>
+    Push
+    </form>
+    </div>
+    </>
+  )
+}
+
+export default Pila
